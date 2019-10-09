5739369a8a6b1840cd5d67f410bb637939fd7cba
Mitch <commandgeneral15@gmail.com>
2019-10-09 12:50:32 +0200

    added ids to entity

--- src/entities/page.entity.ts
+++ src/entities/page.entity.ts
@@ -1,5 +1,5 @@
 import { Layout } from './layout.entity';
-import { Column, Entity, PrimaryGeneratedColumn, OneToOne, JoinColumn, ManyToOne, ManyToMany, Unique, JoinTable } from 'typeorm';
+import { Column, Entity, PrimaryGeneratedColumn, OneToOne, JoinColumn, ManyToOne, ManyToMany, Unique, JoinTable, RelationId } from 'typeorm';
 import { PageCss } from './pageCss.entity';
 
 @Entity()
@@ -24,4 +24,10 @@ export class Page {
   @ManyToMany(type => PageCss, pageCss => pageCss.pages)
   @JoinTable()
   css: PageCss[];
+
+  @RelationId((page: Page) => page.css)
+  cssIds: number[];
+
+  @RelationId((page: Page) => page.layout)
+  layoutId: number;
 }
