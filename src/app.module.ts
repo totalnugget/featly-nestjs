c6ee474da437bc55882a65386bec24f36d7bc156
mitch <commandgeneral15@gmail.com>
2020-01-02 23:57:51 +0100

    parse port as Number

--- src/app.module.ts
+++ src/app.module.ts
@@ -18,7 +18,7 @@ import { ConfigModule } from '@nestjs/config';
     TypeOrmModule.forRoot({
       type: process.env.DB_TYPE as any,
       host: process.env.DB_HOST,
-      port: process.env.DB_PORT as any,
+      port: parseInt(process.env.DB_PORT, 10),
       username: process.env.DB_USER,
       password: process.env.DB_PASSWORD,
       database: process.env.DB_NAME,
